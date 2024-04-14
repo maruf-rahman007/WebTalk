5e2021166a440a0e78d7db9af4acb6a10f937958
maruf-rahman007 <s.marufrahman.g@gmail.com>
2024-04-14 09:53:47 +0600

    fix(frontend):added mobile responsiveness in full blog page

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -17,46 +17,30 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
   }, [blog.content]);
 
   if (!sanitizedContent) {
-    return null; // Or render a loading state
+    return null; 
   }
 
   return (
     <div>
       <Appbar />
-      <div className="flex justify-center">
-        <div className="grid grid-cols-12 px-10 w-full pt-200 max-w-screen-xl pt-12">
-          <div className="col-span-8">
-            <div className="text-5xl font-extrabold">{blog.title}</div>
-            <div className="text-slate-500 pt-4">
-              Posted on 2nd December 2023
-            </div>
-            <div
-              className="pt-4"
-              style={{ whiteSpace: "pre-wrap" }}
-              dangerouslySetInnerHTML={{ __html: sanitizedContent }}
-            />
-          </div>
-          <div className="col-span-4">
-            <div className="text-slate-600 text-lg">Author</div>
-            <div className="flex justify-center">
-              <div className="pr-4 flex justify-center flex-col">
-                <Avater
-                  size="big"
-                  name={blog.author.name || "Anonymous"}
-                />
-              </div>
-              <div>
-                <div className="text-xl font-bold">
-                  {blog.author.name || "Anonymous"}
-                </div>
-                <div className="pt-2 text-slate-500">
-                  Here is some random bio for the author to catch the readers
-                  attention.
-                </div>
+      <div className="px-4 pt-8 max-w-screen-xl mx-auto">
+        <div className="text-3xl font-extrabold">{blog.title}</div>
+        <div className="text-slate-500 pt-2">
+          Posted on 2nd December 2023
+        </div>
+        <div className="pt-8">
+          <div className="text-slate-600 text-lg">Author</div>
+          <div className="flex items-center pt-4">
+            <Avater size="big" name={blog.author.name || "Anonymous"} />
+            <div className="ml-4">
+              <div className="text-xl font-bold">{blog.author.name || "Anonymous"}</div>
+              <div className="text-slate-500">
+                Here is some random bio for the author to catch the readers attention.
               </div>
             </div>
           </div>
         </div>
+        <div className="pt-4" dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
       </div>
     </div>
   );
